ff564793092cd281d34165eb51ca7041311002dd
killertop <31072337+killertop@users.noreply.github.com>
2026-09-08 14:00:47 +0100

    Simplify navigation and rebuild the Vialen About screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -153,7 +153,6 @@ dependencies {
     implementation("com.blacksquircle.ui:language-json:2.6.0")
 
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.3")
-    implementation("com.github.daniel-stoneuk:material-about-library:3.2.0-rc01")
     implementation("com.jakewharton:process-phoenix:2.1.2")
     implementation("com.esotericsoftware:kryo:5.2.1")
     implementation("com.google.guava:guava:31.0.1-android")
